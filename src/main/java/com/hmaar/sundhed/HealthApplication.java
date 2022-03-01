ef75de10f8892fcfe002f15c1bb3a0c9564fa278
AlaaMaHdii <s190434@dtu.dk>
2022-03-01 22:51:40 +0100

    Added graph with dummy data and setup a data controller.

--- src/main/java/com/hmaar/sundhed/HealthApplication.java
+++ src/main/java/com/hmaar/sundhed/HealthApplication.java
@@ -1,18 +1,22 @@
 package com.hmaar.sundhed;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HealthApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HealthApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HealthApplication.class.getResource("app-view.fxml")); // skift til login-view senere hen
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("Vital");
         stage.setScene(scene);
         stage.show();
     }
